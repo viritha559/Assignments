0bda06baa994a24d3a6fbbd3e3602f98aa08b809
viritha <virithakanumuri@gmail.com>
2021-12-30 00:50:52 +0530

    patterns

--- Day6/Pattern5.java
+++ Day6/Pattern5.java
@@ -3,18 +3,34 @@ public class Pattern5 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+    
         for(int i=1;i<=n;i++){
             for(int k=n-1;k>=i;k--){
-                System.out.print("");
+                System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print(j++);
-            }
+                if(j==1 || j==i || i==n){
+                    System.out.print(j+" ");
+
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
-            sc.close();
         }
-        
+        sc.close();
     }
+}
+/*
+output
+PS C:\exercise\Assignments\Day6> java Pattern5      
+5
+    1 
+   1 2
+  1   3
+ 1     4
+1 2 3 4 5
+*/
 
     
